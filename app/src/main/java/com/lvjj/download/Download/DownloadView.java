015fb5ec106ce2062630a4fd2b6f7307471901e8
lvjj <lvi166lvi@gmail.com>
2017-06-07 17:23:04 +0800

    test

---
+++ app/src/main/java/com/lvjj/download/Download/DownloadView.java
@@ -0,0 +1,38 @@
+package com.lvjj.download.Download;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/6/7.
+ */
+
+public class DownloadView extends View {
+    /**
+     * color
+     */
+    private int FRAME_COLOR = 0x236525;
+
+    /**
+     * dimen
+     */
+    private int FRAME_DIMEN = 0x236525;
+
+
+    public DownloadView(Context context) {
+        super(context);
+    }
+
+    public DownloadView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DownloadView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+}
